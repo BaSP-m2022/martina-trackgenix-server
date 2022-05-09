d2d79efca0e553ed20dc75a5042a0f9c5410877d
MartinDidoli <ixanol99@gmail.com>
2022-05-09 20:05:35 -0300

    TG: Function to search timesheet by ID added

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
   res.send(timeSheets);
 });
 
+router.get('/timeSheetId/:id', (req, res) => {
+  const toTimeSheet = parseInt(req.params.id, 10);
+  const defTimeSheet = timeSheets.filter((number) => number.timesheet_id === toTimeSheet);
+  if (defTimeSheet) {
+    res.send(defTimeSheet);
+  } else {
+    res.send('TimeSheet ID not found');
+  }
+});
+
 module.exports = router;
