ffd02031d83a3876a85ad57ad76d493103adc7d7
Evelyn Heredia <evefhrd@gmail.com>
2022-05-10 17:19:12 -0300

    TG: changes after pre-entrega meeting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // use "import" to import libraries
 import express from 'express';
+import tasksRouter from './resources/tasks';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
-const tasksRouter = require('./resources/tasks');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use('/tasks', tasksRouter);
--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const fileSystem = require('fs');
-const tasks = require('../data/tasks.json');
+import { Router } from 'express';
+import { writeFile } from 'fs';
+import tasks from '../data/tasks.json';
 
-const router = express.Router();
+const tasksRouter = Router();
+
 // Obtain whole list of tasks
-router.get('/getAll', (req, res) => {
+tasksRouter.get('/', (req, res) => {
   res.send(tasks);
 });
+
+// Obtain a task by description and by id
+tasksRouter.get('/:id', (req, res) => {
+  const requiredTask = parseInt(req.params.id, 10);
+  const foundTask = tasks.find((task) => task.id === requiredTask);
+  if (requiredTask && foundTask) {
+    res.json(tasks.filter((task) => task.id === requiredTask));
+  } else {
+    res.json({ msg: `${req.params.id} was not found` });
+  }
+});
+
 // Create a task
-router.post('/', (req, res) => {
+tasksRouter.post('/', (req, res) => {
   const taskData = req.body;
   if (taskData.id && taskData.description) {
     tasks.push(taskData);
-    fileSystem.writeFile('src/data/tasks.json', JSON.stringify(tasks), (err) => {
+    writeFile('src/data/tasks.json', JSON.stringify(tasks), (err) => {
       if (err) {
         res.send(err);
       } else {
@@ -23,28 +36,15 @@ router.post('/', (req, res) => {
     res.send('Task data is incomplete');
   }
 });
-// Obtain a task by description and by id
-router.get('/', (req, res) => {
-  const requiredTaskId = parseInt(req.query.id, 10);
-  const foundTaskId = tasks.some((task) => task.id === requiredTaskId);
-  const requiredTask = req.query.description;
-  const foundTaskDesc = tasks.some((description) => description.description === requiredTask);
-  if (requiredTaskId && foundTaskId) {
-    res.json(tasks.filter((task) => task.id === requiredTaskId));
-  } else if (requiredTask && foundTaskDesc) {
-    res.json(tasks.filter((description) => description.description === requiredTask));
-  } else {
-    res.json({ msg: `${req.query.description} was not found` });
-  }
-});
+
 // Delete a task
-router.delete('/:id', (req, res) => {
+tasksRouter.delete('/:id', (req, res) => {
   const requiredTask = parseInt(req.params.id, 10);
   const foundTask = tasks.filter((task) => task.id !== requiredTask);
   if (foundTask.length === tasks.length) {
     res.send(`Could not delete task id: ${req.params.id} because it was not found`);
   } else {
-    fileSystem.writeFile('src/data/tasks.json', JSON.stringify(foundTask), (err) => {
+    writeFile('src/data/tasks.json', JSON.stringify(foundTask), (err) => {
       if (err) {
         res.send(err);
       } else {
@@ -53,8 +53,9 @@ router.delete('/:id', (req, res) => {
     });
   }
 });
+
 // Edit a task
-router.put('/:id', (req, res) => {
+tasksRouter.put('/:id', (req, res) => {
   const requiredTask = parseInt(req.params.id, 10);
   const foundTask = tasks.find((task) => task.id === requiredTask);
   if (foundTask) {
@@ -63,7 +64,7 @@ router.put('/:id', (req, res) => {
       const updTask = data;
       if (updTask.id === requiredTask) {
         updTask.description = taskData.description ? taskData.description : data.description;
-        fileSystem.writeFile('src/data/tasks.json', JSON.stringify(tasks), (err) => {
+        writeFile('src/data/tasks.json', JSON.stringify(tasks), (err) => {
           if (err) {
             res.send(err);
           } else {
@@ -77,4 +78,4 @@ router.put('/:id', (req, res) => {
   }
 });
 
-module.exports = router;
+export default tasksRouter;
