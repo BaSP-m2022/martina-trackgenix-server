a659f9e2e321b9bcad703e933caa53ccccb67244
Rodrigo <99558370+x-Ramses-x@users.noreply.github.com>
2022-05-16 05:19:51 -0300

    TG: Update models

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -2,16 +2,14 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const projectSchema = new Schema({
+const projectsSchema = new Schema({
   project_name: { type: String, required: true },
   start_date: { type: Date, required: true },
   finish_date: { type: Date, required: false },
   client: { type: String, required: true },
-  active: { type: Boolean },
-  employees: {
-    name: { type: String, required: true },
-    role: { type: String, required: true, enum: ['DEV', 'QA', 'PM', 'TL'] },
-  },
+  active: { type: Boolean, required: true },
+  employees: { type: [Object], required: true },
+  admin_id: { type: Number, required: true },
 });
 
-export default mongoose.model('Project', projectSchema);
+export default mongoose.model('Projects', projectsSchema);
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -5,12 +5,9 @@ import projectsValidation from '../validations/projects';
 
 const router = express.Router();
 
-// .use('./projects');
-
 router
     .get('/',projectsConstroller.getAllProjects);
     .post('/',projectsValidation.validateCreation, projectsConstroller.createProject);
-    .get('/',projectsConstroller.getAllProjects);
 
 export default router;
 */
