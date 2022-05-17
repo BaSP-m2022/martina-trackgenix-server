9270326f20bfb12f60d056d5417248b878099b60
Julian Lescano <lescano.julian22@gmail.com>
2022-05-17 20:17:36 -0300

    TG: Routes index fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import adminRoutes from './admins';
 import timeSheetRoutes from './time-sheets';
 
 const router = express.Router();
 
 router
+  .use('/admins', adminRoutes)
   .use('/time-sheet', timeSheetRoutes);
 
 export default router;
