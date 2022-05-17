53c21c1a8754d94930cc3115102fb10cd4c95574
Rodrigo <99558370+x-Ramses-x@users.noreply.github.com>
2022-05-16 22:48:18 -0300

    TG: Update endpoints logic including HTTP status code - GET & POST

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,97 +1,38 @@
-import { Router } from 'express';
-import fileSystem from 'fs';
-import projects from '../data/projects.json';
+import projectsSchema from '../data/projects.json';
 
-const projectsRoutes = Router();
-
-projectsRoutes.get('/getAll', (req, res) => {
-  res.send(projects);
-});
-
-projectsRoutes.post('/add', (req, res) => {
-  const projectsData = req.body;
-  projects.push(projectsData);
-  fileSystem.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send('Project created');
-    }
-  });
-});
-
-projectsRoutes.put('/:id', (req, res) => {
-  const projectId = parseInt(req.params.id, 10);
-  const found = projects.find((project) => project.id === projectId);
-  if (found) {
-    const proData = req.body;
-    projects.forEach((project) => {
-      const updPro = project;
-      if (updPro.id === projectId) {
-        updPro.project_name = proData.project_name ? proData.project_name : project.project_name;
-        updPro.start_date = proData.start_date ? proData.start_date : project.start_date;
-        updPro.finish_date = proData.finish_date ? proData.finish_date : project.finish_date;
-        updPro.client = proData.client ? proData.client : project.client;
-        updPro.active = proData.active === updPro.active ? project.active : proData.active;
-        updPro.admin_id = proData.admin_id ? proData.admin_id : project.admin_id;
-        fileSystem.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-          if (err) {
-            res.send(err);
-          } else {
-            res.send(`Project ${req.params.id} edited`);
-          }
-        });
-      }
+const getAllProjects = async (req, res) => {
+  try {
+    const allProjects = await projectsSchema.find({});
+    return res.status(200).json(allProjects);
+  } catch (err) {
+    return res.status(500).json({
+      msg: 'An error has occurred',
     });
-  } else {
-    res.send(`Project with id: ${req.params.id} does not exist`);
   }
-});
+};
 
-projectsRoutes.get('/:id', (req, res) => {
-  const found = projects.some((project) => project.id === parseInt(req.params.id, 10));
-  if (found) {
-    res.json(projects.filter((project) => project.id === parseInt(req.params.id, 10)));
-  } else {
-    res.status(400).json({ msg: `Not a project with id: ${req.params.id}` });
+const createProject = async (req, res) => {
+  try {
+    const project = await projectsSchema.create({
+      project_name: req.body.project_name,
+      start_date: req.body.start_date,
+      finish_date: req.body.finish_date,
+      client: req.body.client,
+      active: req.body.active,
+      employees: req.body.employees,
+      admin_id: req.body.admin_id,
+    });
+    const result = await project.save();
+    return res.status(201).json(result);
+  } catch (error) {
+    return res.json({
+      msg: 'An error has occured',
+      error: error.details[0].message,
+    });
   }
-});
+};
 
-projectsRoutes.get('/', (req, res) => {
-  const projectName = req.query.project_name;
-  const filterName = projects.some((project) => project.project_name === projectName);
-  const projectDate = req.query.start_date;
-  const filterDate = projects.some((project) => project.start_date === projectDate);
-  const projectClient = req.query.client;
-  const filterClient = projects.some((project) => project.client === projectClient);
-  const projectActive = req.query.active;
-  const filterAct = projects.some((project) => JSON.stringify(project.active) === projectActive);
-  if (projectName && filterName) {
-    res.json(projects.filter((project) => project.project_name === projectName));
-  } else if (projectDate && filterDate) {
-    res.json(projects.filter((project) => project.start_date === projectDate));
-  } else if (projectActive && filterAct) {
-    res.json(projects.filter((project) => JSON.stringify(project.active) === projectActive));
-  } else if (projectClient && filterClient) {
-    res.json(projects.filter((project) => project.client === projectClient));
-  } else {
-    res.send('Project not found');
-  }
-});
-
-projectsRoutes.delete('/:id', (req, res) => {
-  const projectId = parseInt(req.params.id, 10);
-  const filterProject = projects.filter((project) => project.id !== projectId);
-  if (projects.length === filterProject.length) {
-    res.send('The project is not delete because it was not found');
-  }
-  fileSystem.writeFile('src/data/projects.json', JSON.stringify(filterProject), (error) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send('Project deleted');
-    }
-  });
-});
-
-export default projectsRoutes;
+export default {
+  getAllProjects,
+  createProject,
+};
