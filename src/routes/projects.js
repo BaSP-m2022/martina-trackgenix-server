546a429a6d0d30130f9fc34b70372bdb0ea20bc8
Rodrigo <99558370+x-Ramses-x@users.noreply.github.com>
2022-05-17 12:32:31 -0300

    TG: Create Postman Collection - Delete JSON file

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import projectsConstroller from '../controllers/projects';
+import projectsController from '../controllers/projects';
 import projectsValidation from '../validations/projects';
 
 const router = express.Router();
 
 router
-  .get('/', projectsConstroller.getAllProjects)
-  .post('/', projectsValidation, projectsConstroller.createProject);
+  .get('/', projectsController.getAllProjects)
+  .post('/', projectsValidation, projectsController.createProject);
 
 export default router;
