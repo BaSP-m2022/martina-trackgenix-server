6603dd6062ac28ef655582cabaf4d95cedfd0dcd
Rodrigo <99558370+x-Ramses-x@users.noreply.github.com>
2022-05-17 21:18:08 -0300

    Update routes index and postman collection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import projectRoutes from './projects';
+import adminRoutes from './admins';
 
 const router = express.Router();
 
 router.use('/projects', projectRoutes);
+router.use('/admins', adminRoutes);
 
 export default router;
