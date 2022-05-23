df00accbd73fcb78b87bdaf37e940036ef1a06bb
Julian Lescano <lescano.julian22@gmail.com>
2022-05-22 22:58:16 -0300

    TG: Update project enpoint test in progress

--- src/seeds/projects.js
+++ src/seeds/projects.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default [{
-  _id: mongoose.Types.ObjectId('6283baefcd44998f831522aa'),
+  id: mongoose.Types.ObjectId('6283baefcd44998f831522aa'),
   project_name: 'The Little Prince',
   start_date: '1943-01-06T03:00:00.000Z',
   finish_date: '1943-04-06T03:00:00.000Z',
@@ -11,22 +11,8 @@ export default [{
     {
       role: 'DEV',
       rate: '10',
-      _id: '6283baefcd44998f831522ab',
-    },
-    {
-      role: 'QA',
-      rate: '50',
-      _id: '6283baefcd44998f831522ac',
-    },
-    {
-      role: 'TL',
-      rate: '60',
-      _id: '6283baefcd44998f831522ad',
-    },
-    {
-      role: 'DEV',
-      rate: '85',
-      _id: '6283baefcd44998f831522ae',
+      _id: 12,
     },
   ],
+  admin_id: '43',
 }];
--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -6,10 +6,68 @@ import app from '../app';
 beforeAll(async () => {
   await Projects.collection.insertMany(projectSeed);
 });
-
-describe('GET', () => {
-  test('response should return a 200 status', async () => {
+let projectId;
+describe('Test GET /projects', () => {
+  test('Response should return a 200 status', async () => {
     const response = await request(app).get('/projects').send();
+    expect(response.status).toBe(200);
+  });
+  test('Response should return error false', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.error).toBeFalsy();
+  });
+  test('Response should return at least one employee', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+  test('Response should return message Project found', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.body.message).toBe('Project found');
+  });
+});
+
+describe('Test POST /projects', () => {
+  test('Project should be created', async () => {
+    const response = await request(app).post('/projects').send({
+      project_name: 'The Little Prince',
+      start_date: '1943-01-06T03:00:00.000Z',
+      finish_date: '1943-04-06T03:00:00.000Z',
+      client: 'Antoine de Saint-Exupéry',
+      active: true,
+      employees: [
+        {
+          role: 'DEV',
+          rate: '10',
+          id: 12,
+        },
+      ],
+      admin_id: '43',
+    });
+
+    expect(response.status).toBe(201);
+    // eslint-disable-next-line no-underscore-dangle
+    projectId = response.body.data._id;
+  });
+});
+
+describe('Test PUT ', () => {
+  test('project should be updated', async () => {
+    const response = await request(app).put(`/projects/${projectId}`).send({
+      project_name: 'The Little pepe',
+      start_date: '1943-01-06T03:00:00.000Z',
+      finish_date: '1943-04-06T03:00:00.000Z',
+      client: 'Antoine de Saint-Exupéry',
+      active: false,
+      employees: [
+        {
+          role: 'DEV',
+          rate: '10',
+          id: 12,
+        },
+      ],
+      admin_id: '43',
+    });
+
     expect(response.status).toBe(200);
   });
 });
