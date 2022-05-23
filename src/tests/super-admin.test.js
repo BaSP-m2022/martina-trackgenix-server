75a3894b2c9f964c01b130a2c8016a7153eb25a3
lautaro acosta <lautaroeacosta23@gmail.com>
2022-05-23 10:59:29 -0300

    TG: super admin unit test of post

--- src/tests/super-admin.test.js
+++ src/tests/super-admin.test.js
@@ -23,3 +23,49 @@ describe('GET', () => {
     expect(response.body.data.length).toBeGreaterThan(0);
   });
 });
+
+describe('POST', () => {
+  test('should create an super admin', async () => {
+    const response = await request(app).post('/super-admins').send({
+      firstName: 'Lautaro',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.status).toBe(201);
+    // superAdmId = response.body.data._id;
+  });
+
+  test('message should indicate the creation of the super admin', async () => {
+    const response = await request(app).post('/super-admins').send({
+      firstName: 'Lautaro',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.body.message).toEqual('super Admin created');
+  });
+
+  test('should not create an super admin when one field was not created', async () => {
+    const response = await request(app).post('/super-admins').send({
+      firstName: '',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('should not create super admin', async () => {
+    const response = await request(app).post('/super-admins').send();
+    expect(response.status).toBe(400);
+  });
+});
+
+// describe('DELETE /:id', () => {
+//     const response = await request(app).delete(`/super-admins/${superAdmId}`).send();
+//     expect(response.status)
+// })
