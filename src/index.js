6ced4fb7c74ae4bc51454f7c1d0929040b74d843
Rodrigo <99558370+x-Ramses-x@users.noreply.github.com>
2022-05-22 22:08:27 -0300

    TG: seeds times-sheets, app file, correction of index routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import mongoose from 'mongoose';
-import router from './routes';
+import app from './app';
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
 const URL = process.env.DATABASE_URL;
-
-app.use(express.json());
-app.use(router);
 
 mongoose.connect(URL, (error) => {
   if (error) {
