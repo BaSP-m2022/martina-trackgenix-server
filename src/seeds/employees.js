43dd6d873e3e91f89aee34a2a404e7c1f0e2e75d
Laura Brussa <mlaurafbrussa@gmail.com>
2022-05-22 23:49:48 -0300

    TG: test and seed folder and mock data base

---
+++ src/seeds/employees.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('628af2c4b6824cd901ba98b8'),
+  first_name: 'Laura',
+  last_name: 'Brussa',
+  phone: '1234567',
+  email: 'laura@brussa.com',
+  password: 'testeo123',
+  active: true,
+}];
