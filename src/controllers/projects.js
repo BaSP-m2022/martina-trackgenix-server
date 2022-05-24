a8e9914b37003bc9b8ab1561e9cb3c6827d23725
Evelyn Heredia <evefhrd@gmail.com>
2022-05-24 15:47:42 -0300

    TG: postman collection added

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,6 +1,71 @@
 import Project from '../models/Projects';
 
-// EDIT PROJECT BY ID
+const createProject = async (req, res) => {
+  try {
+    const project = req.body;
+    const newProject = await Project.create(project);
+    return res.status(201).json({
+      message: 'Project created',
+      data: newProject,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: 'An error has ocurred',
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const getAllProjects = async (req, res) => {
+  try {
+    const allProjects = await Project.find({});
+    return res.status(200).json({
+      message: 'Project found',
+      data: allProjects,
+      error: false,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: 'An error has occurred',
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const getProjectById = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const project = await Project.findById(req.params.id);
+      if (!project) {
+        return res.status(404).json({
+          message: 'Project not found',
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Project found',
+        data: project,
+        error: false,
+      });
+    }
+    return res.status(400).json({
+      message: 'Invalid params',
+      data: undefined,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
 const updateProject = async (req, res) => {
   try {
     const result = await Project.findByIdAndUpdate(
@@ -36,7 +101,6 @@ const updateProject = async (req, res) => {
   }
 };
 
-// DELETE PROJECT
 const deleteProject = async (req, res) => {
   try {
     const projectId = await Project.findByIdAndDelete(req.params.id);
@@ -57,69 +121,9 @@ const deleteProject = async (req, res) => {
   }
 };
 
-const getAllProjects = async (req, res) => {
-  try {
-    const allProjects = await Project.find({});
-    return res.status(200).json({
-      message: 'Project found',
-      data: allProjects,
-      error: false,
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: 'An error has occurred',
-      data: undefined,
-      error: true,
-    });
-  }
-};
-
-const getProjectById = async (req, res) => {
-  try {
-    const projectId = await Project.findOne(req.param.id);
-    if (!projectId) {
-      return res.status(404).json({
-        message: 'Project not found',
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: 'Project found',
-      data: projectId,
-      error: false,
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: 'An error has ocurred',
-      data: undefined,
-      error: true,
-    });
-  }
-};
-
-const createProject = async (req, res) => {
-  try {
-    const project = req.body;
-    const newProject = await Project.create(project);
-
-    return res.status(201).json({
-      message: 'Project created',
-      data: newProject,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      message: 'An error has ocurred',
-      data: undefined,
-      error: true,
-    });
-  }
-};
-
 export default {
-  getAllProjects,
   createProject,
+  getAllProjects,
   getProjectById,
   updateProject,
   deleteProject,
--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -34,6 +34,29 @@ beforeAll(async () => {
 
 let projectId;
 
+describe('Test POST /projects', () => {
+  test('Response should return a 201 status, error false, and message Project created', async () => {
+    const response = await request(app).post('/projects').send(mockRequestBody);
+    expect(response.status).toBe(201);
+    expect(response.error).toBeFalsy();
+    expect(response.body.data.message).toEqual('Project created');
+    projectId = response.body.data._id;
+  });
+
+  test('Project should NOT be created if the request is empty', async () => {
+    const response = await request(app).post('/projects').send();
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+    expect(response.body.message).toEqual('An error has ocurred');
+  });
+
+  test('Response should return a 400 status, error true if at least one required field is empty', async () => {
+    const response = await request(app).post('/projects').send(mockRequestBodyIncomplete);
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+});
+
 describe('Test GET /projects', () => {
   test('Response should return a 200 status, error false, and message Project found', async () => {
     const response = await request(app).get('/projects').send();
@@ -51,30 +74,22 @@ describe('Test GET /projects', () => {
 
 describe('Test GET /projects/:id', () => {
   test('Response should return a 200 status, error false, and message Project found', async () => {
-    const response = await request(app).get(`/projects/${projectId}`).send();
+    const response = await request(app).get(`/projects/${projectId}`);
     expect(response.status).toBe(200);
     expect(response.error).toBeFalsy();
     expect(response.body.message).toEqual('Project found');
   });
-});
 
-describe('Test POST /projects', () => {
-  test('Response should return a 201 status, error false, and message Project created', async () => {
-    const response = await request(app).post('/projects').send(mockRequestBody);
-    expect(response.status).toBe(201);
-    expect(response.error).toBeFalsy();
-    expect(response.body.message).toEqual('Project created');
-    projectId = response.body.data._id;
+  test('Response should return a 404 status, error true, and message Project not found if the ID is inexistent', async () => {
+    const response = await request(app).get('/projects/6283baefcd44998f831522bb').send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+    expect(response.body.message).toEqual('Project not found');
   });
 
-  test('Project should NOT be created if the request is empty', async () => {
-    const response = await request(app).post('/projects').send();
-    expect(response.status).toBe(400);
-  });
-
-  test('Response should return a 400 status, error true if at least one required field is empty', async () => {
-    const response = await request(app).post('/projects').send(mockRequestBodyIncomplete);
-    expect(response.status).toBe(400);
+  test('Response should return a 500 status, error true if the ID format is invalid', async () => {
+    const response = await request(app).get('/projects/localhost:3000/projects/6283baefcd44998f831522bb3333333333').send();
+    expect(response.status).toBe(404);
     expect(response.error).toBeTruthy();
   });
 });
