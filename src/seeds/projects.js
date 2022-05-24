7c7a62af1d88f0e7946bb0e519f20f0f2c982ce2
Evelyn Heredia <evefhrd@gmail.com>
2022-05-24 17:46:44 -0300

    TG: POST unit testing edited

--- src/seeds/projects.js
+++ src/seeds/projects.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default [{
-  _id: mongoose.Types.ObjectId('6283baefcd44998f831522aa'),
+  id: mongoose.Types.ObjectId('6283baefcd44998f831522aa'),
   project_name: 'The Little Prince',
   start_date: '1943-01-06T03:00:00.000Z',
   finish_date: '1943-04-06T03:00:00.000Z',
@@ -11,8 +11,7 @@ export default [{
     {
       role: 'DEV',
       rate: '10',
-      _id: 12,
+      id: '628578f0b38934591452aa2e',
     },
   ],
-  admin_id: '43',
 }];
--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import Project from '../models/Projects';
 import projectsSeed from '../seeds/projects';
@@ -14,18 +13,23 @@ const mockRequestBody = {
     {
       role: 'DEV',
       rate: '10',
-      id: 12,
+      id: '628578f0b38934591452aa2e',
     },
   ],
-  admin_id: '43',
 };
 
 const mockRequestBodyIncomplete = {
-  project_name: 'The Little Prince',
   start_date: '1943-01-06T03:00:00.000Z',
   finish_date: '1943-04-06T03:00:00.000Z',
   client: 'Antoine de Saint-Exupéry',
-  admin_id: '43',
+  active: false,
+  employees: [
+    {
+      role: 'DEV',
+      rate: '10',
+      id: '628578f0b38934591452aa2e',
+    },
+  ],
 };
 
 beforeAll(async () => {
@@ -38,22 +42,22 @@ describe('Test POST /projects', () => {
   test('Response should return a 201 status, error false, and message Project created', async () => {
     const response = await request(app).post('/projects').send(mockRequestBody);
     expect(response.status).toBe(201);
-    expect(response.error).toBeFalsy();
-    expect(response.body.data.message).toEqual('Project created');
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toEqual('Project created');
+    // eslint-disable-next-line no-underscore-dangle
     projectId = response.body.data._id;
   });
 
   test('Project should NOT be created if the request is empty', async () => {
     const response = await request(app).post('/projects').send();
     expect(response.status).toBe(400);
-    expect(response.error).toBeTruthy();
-    expect(response.body.message).toEqual('An error has ocurred');
+    expect(response.body.error).toBeTruthy();
   });
 
   test('Response should return a 400 status, error true if at least one required field is empty', async () => {
     const response = await request(app).post('/projects').send(mockRequestBodyIncomplete);
     expect(response.status).toBe(400);
-    expect(response.error).toBeTruthy();
+    expect(response.body.error).toBeTruthy();
   });
 });
 
