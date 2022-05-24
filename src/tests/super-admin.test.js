e236f78b7808cbc6c28917df5ba4bd02ad2db726
lautaro acosta <lautaroeacosta23@gmail.com>
2022-05-23 23:37:20 -0300

    TG: add test of get by id, put and delete

--- src/tests/super-admin.test.js
+++ src/tests/super-admin.test.js
@@ -7,22 +7,7 @@ beforeAll(async () => {
   await SuperAdmins.collection.insertMany(superAdmSeed);
 });
 
-describe('GET', () => {
-  test('response should return a 200 status', async () => {
-    const response = await request(app).get('/super-admins').send();
-    expect(response.status).toBe(200);
-  });
-
-  test('response should return false error', async () => {
-    const response = await request(app).get('/super-admins').send();
-    expect(response.error).toBe(false);
-  });
-
-  test('response should return at least one super admin', async () => {
-    const response = await request(app).get('/super-admins').send();
-    expect(response.body.data.length).toBeGreaterThan(0);
-  });
-});
+let superAdmId;
 
 describe('POST', () => {
   test('should create an super admin', async () => {
@@ -34,7 +19,8 @@ describe('POST', () => {
       active: true,
     });
     expect(response.status).toBe(201);
-    // superAdmId = response.body.data._id;
+    // eslint-disable-next-line no-underscore-dangle
+    superAdmId = response.body.data._id;
   });
 
   test('message should indicate the creation of the super admin', async () => {
@@ -65,7 +51,94 @@ describe('POST', () => {
   });
 });
 
-// describe('DELETE /:id', () => {
-//     const response = await request(app).delete(`/super-admins/${superAdmId}`).send();
-//     expect(response.status)
-// })
+describe('GET', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/super-admins').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return false error', async () => {
+    const response = await request(app).get('/super-admins').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('response should return at least one super admin', async () => {
+    const response = await request(app).get('/super-admins').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+});
+
+describe('GET BY ID /:id', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get(`/super-admins/${superAdmId}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return false error', async () => {
+    const response = await request(app).get(`/super-admins/${superAdmId}`).send();
+    expect(response.error).toBe(false);
+  });
+
+  test('response should return at least one super admin', async () => {
+    const response = await request(app).get('/super-admins').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+});
+
+describe('PUT /:id', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).put(`/super-admins/${superAdmId}`).send({
+      firstName: 'Lautaro',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.status).toBe(201);
+  });
+
+  test('message should indicate the update of the super admin', async () => {
+    const response = await request(app).put(`/super-admins/${superAdmId}`).send({
+      firstName: 'Lautaro',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.body.message).toBe('Super Admin Updated');
+  });
+
+  test('should not update the super admin when one field was not completed', async () => {
+    const response = await request(app).post('/super-admins').send({
+      firstName: '',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23@gmail.com',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('email field should be valid', async () => {
+    const response = await request(app).post('/super-admins').send({
+      firstName: 'Lautaro',
+      lastName: 'Acosta',
+      email: 'lautaroeacosta23gmailcom',
+      password: 'test12345',
+      active: true,
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('update super admin has an error', async () => {
+    const response = await request(app).post('/super-admins').send();
+    expect(response.status).toBe(400);
+  });
+});
+
+describe('DELETE /:id', () => {
+  test('should delete a super admin', async () => {
+    const response = await request(app).delete(`/super-admins/${superAdmId}`).send();
+    expect(response.status).toBe(204);
+  });
+});
