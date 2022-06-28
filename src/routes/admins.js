e43c97f4be5c040e91e9589705fef229da56eb06
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-27 23:05:02 -0300

    TG-92: Updated the edit controller

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import adminsController from '../controllers/admins';
 import adminsValidation from '../validations/admins';
+import authController from '../controllers/auth';
 
 const router = express.Router();
 
 router
   .post('/', adminsValidation, adminsController.createAdmin)
-  .get('/', adminsController.getAllAdmins)
+  .get('/', adminsController.getAllAdmins, authController.getAuth)
   .get('/getByName', adminsController.getAdminByName)
   .get('/getBySurname', adminsController.getAdminByLastName)
   .get('/:id', adminsController.getAdminById)
