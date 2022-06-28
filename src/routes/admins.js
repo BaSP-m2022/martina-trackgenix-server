f16d91086f97b16fc20fca02e291d3ba4011d24f
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-28 16:30:03 -0300

    TG-92: Corrected a thing

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import adminsController from '../controllers/admins';
 import adminsValidation from '../validations/admins';
-import authController from '../controllers/auth';
 
 const router = express.Router();
 
 router
   .post('/', adminsValidation, adminsController.createAdmin)
-  .get('/', adminsController.getAllAdmins, authController.getAuth)
+  .get('/', adminsController.getAllAdmins)
   .get('/getByName', adminsController.getAdminByName)
   .get('/getBySurname', adminsController.getAdminByLastName)
   .get('/:id', adminsController.getAdminById)
